f7dc8c2fd6a2b165954ff5ae5a63c2cbba473b7b
Zeyu Lu <zylu@yonsei.ac.kr>
2022-10-23 18:47:37 +0900

    first commit

---
+++ phy.py
@@ -0,0 +1,19 @@
+MidtermRawGrade = input("Midterm Grade: ")
+
+MidtermRawGrade = float(MidtermRawGrade)
+
+FinalRawGrade = input("Final Grade: ")
+
+FinalRawGrade = float(FinalRawGrade)
+
+HomeworkRawGrade = input("Homework Grade: ")
+
+HomeworkRawGrade = float(HomeworkRawGrade)
+
+LabReport = input("Lab Report Grade: ")
+
+LabReport = float(LabReport)
+
+FinalGrade = (MidtermRawGrade * 0.25) + (FinalRawGrade * 0.35) + (HomeworkRawGrade * 0.15) + (LabReport * 0.25)
+
+print("Your final grade is: ", FinalGrade)
